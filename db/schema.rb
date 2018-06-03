60cdfbe74b0b498439ab8145e28ae59bf3870872
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-06-03 11:38:52 +0100

    done set to false by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180601052551) do
+ActiveRecord::Schema.define(version: 20180602175303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180601052551) do
     t.date "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "done", default: false, null: false
   end
 
 end
